1fd1528f98ff040f123f8d10020e24a8f57d2bd2
Strangz <lthai@paragoniu.edu.kh>
2025-09-17 15:43:48 -0500

    Fix Navbar import path

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,13 @@
 import StarBackground  from "@/components/StarBackground";
 import ThemeToggle from "@/components/ThemeToggle";
-import { NavBar } from "../components/Navbar";
+import { NavBar } from "@/components/Navbar";
 import { HeroSection } from "@/components/HeroSection";
-import { AboutMe } from "../components/AboutMe";
-import { SkillsSection } from "../components/SkillsSection";
-import { ProjectsSection } from "../components/ProjectsSection";
-import { ContactSection } from "../components/ContactSection";
-import { EducationSection } from "../components/EducationSection";
-import { Footer } from "../components/Footer";
+import { AboutMe } from "@/components/AboutMe";
+import { SkillsSection } from "@/components/SkillsSection";
+import { ProjectsSection } from "@/components/ProjectsSection";
+import { ContactSection } from "@/components/ContactSection";
+import { EducationSection } from "@/components/EducationSection";
+import { Footer } from "@/components/Footer";
 
 export const Home = () => {
   return (
